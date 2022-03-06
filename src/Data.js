edd13eaf5d2839e224d8d33a26b01a496bf97113
Fatemakhan05 <93704892+Fatemakhan05@users.noreply.github.com>
2022-03-06 21:09:37 +0100

    Add files via upload
    finish first portfolio

---
+++ src/Data.js
@@ -0,0 +1,34 @@
+export const products = [
+  {
+    id: 1,
+    image:
+      "amazon.png",
+    link: " https://amazon-clone-ap.netlify.app/"
+  },
+  {
+    id: 2,
+    image:
+      "spotify.png",
+    link: "https://open.spotify.com/"
+  },
+  {
+    id: 3,
+    image: "todolist.png",
+    link: "http://lama.dev"
+  },
+  {
+    id: 4,
+    image: "screenshot.png",
+    link: "http://lama.dev"
+  },
+  {
+    id: 5,
+    image: "https://images2.minutemediacdn.com/image/upload/c_fill,w_720,ar_16:9,f_auto,q_auto,g_auto/shape/cover/sport/dataimagejpegbase649j4AAQSkZJRgABAQAAAQABAAD2wBDAA-2b49722eb7658f7abb75f33e3a7b80ee.jpg",
+    link: "https://www.skysports.com/"
+  },
+  {
+    id: 6,
+    image: " todolist.png",
+    link: "http://lama.dev"
+  }
+];
